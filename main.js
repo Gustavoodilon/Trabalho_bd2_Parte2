c55765672c76ba341a69ff58d0b003b635869275
Gustavo Odilon Da Costa <gustavoodilonda@gmail.com>
2026-03-31 21:07:50 -0300

    lendo todas as linhas do csv e o csv

--- main.js
+++ main.js
@@ -1,21 +1,25 @@
 const { Sequelize, DataTypes } = require('sequelize');
+const fs = require('fs');
+const readline = require('readline');
 
-// Configuração do Banco de Dados
+let results = [];
+
+// Configuração do Banco
 const HOST = "mysql-209830-0.cloudclusters.net";
 const USER = "admin";
 const PASSWORD = "81DvDok0";
 const DATABASE = "sakila";
 const PORT = 19121;
 
-const sequelize = new Sequelize(DATABASE, USER, PASSWORD, { 
-    host: HOST, 
-    dialect: "mysql", 
-    port: PORT, 
-    logging: false 
+const sequelize = new Sequelize(DATABASE, USER, PASSWORD, {
+    host: HOST,
+    dialect: "mysql",
+    port: PORT,
+    logging: false
 });
 
-//armazenando titulos de profissoes unicos para evitar redundancia de dados
-const job = sequelize.define('job', {
+// tabela de cargos
+const Job = sequelize.define('job', {
     job_id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
@@ -23,40 +27,80 @@ const job = sequelize.define('job', {
     },
     job_title: {
         type: DataTypes.STRING,
-        primaryKey: true,
-        unique: true //garante que cada cargo seja único
-   }
+        unique: true
+    }
 }, {
-        tableName: 'jobs',
-        timestamps: false
-    });
+    tableName: 'jobs',
+    timestamps: false
+});
 
-const pessoa = sequelize.define('pessoa', {
+// tabela de pessoas
+const Pessoa = sequelize.define('pessoa', {
     id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
         autoIncrement: true
     },
-    pessoa_id: {
-        type: DataTypes.INTEGER,
+    user_id: {
+        type: DataTypes.STRING,
         allowNull: false,
-        unique: true //garante que cada pessoa tenha um user.id único
+        unique: true
     },
-    first_name: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    last_name: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    sex: {
-        type: DataTypes.STRING,
-    },
-    email: {
-        type: DataTypes.STRING,
-    },
-    phone: {
-        type: DataTypes.STRING,
-    },
+    first_name: DataTypes.STRING,
+    last_name: DataTypes.STRING,
+    sex: DataTypes.STRING,
+    email: DataTypes.STRING,
+    phone: DataTypes.STRING,
+    date_of_birth: DataTypes.DATE
 });
+
+const headers = [
+    'Index',
+    'UserId',
+    'FirstName',
+    'LastName',
+    'Sex',
+    'Email',
+    'Phone',
+    'DateOfBirth',
+    'JobTitle'
+];
+
+const rl = readline.createInterface({
+    input: fs.createReadStream('people-100000.csv'),
+    crlfDelay: Infinity
+});
+
+let isFirstLine = true;
+
+let contador = 0;
+
+rl.on('line', (line) => {
+
+  if (isFirstLine) {
+    isFirstLine = false;
+    return;
+  }
+
+  line = line.replace(/^"|"$/g, '');
+
+  const values = line.split(',');
+
+  const obj = {};
+
+  headers.forEach((header, index) => {
+    obj[header] = values[index];
+  });
+
+  results.push(obj);
+
+  contador++;
+
+   // if (contador <= 100000) {
+   //     console.log("Registro separado:");
+        console.log(obj);
+   // }
+
+});
+
+console.log(results);
